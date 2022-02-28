19081bd997753815c52a60563ac6dacd572c06b3
Jonni Rainisto <jonni.rainisto@gmail.com>
2022-02-28 02:02:27 +0200

    config for mute sounds toggle

--- code/C64MultiKernel.c
+++ code/C64MultiKernel.c
@@ -2,7 +2,6 @@
 // coded by BWACK in mikroC, rewritten by TEBL for single LED.
 // SX-64 reset logic (inspired by SX-64 Ultra Reset) by Jonni
 
-
 // Multikernel switcher for the C64 breadbin/longboard
 // a 2332 ROM to 27C256 ROM adapter with four kernels
 // The microcontroller program flips through the four kernels by holding down
@@ -19,8 +18,8 @@
 // 4MHz Intosc - I/O on GP4
 // All functions disabled (MCLR, brownout etc.)
 
-
 // changes:
+//   2022-02-28 Rev 2.0.3 - Added config for mute toggle
 //   2022-01-27 Rev 2.0.2 - Added config for wait time toggle
 //   2022-01-26 Rev 2.0.1 - Added config for start- and reset-sound toggle
 //   2022-01-03 Rev 2.0  - Rewritten for SX-64 reset logic
@@ -48,6 +47,7 @@
 #define RESETSOUND_TOGGLE 6
 #define STARTSOUND_TOGGLE 7
 #define WAITTIME_TOGGLE   8
+#define MUTE_TOGGLE       9
 
 char STATE=IDLE_STATE;
 char cycle=0,buttontimer=0, old_button;
@@ -56,6 +56,7 @@ char driveno=0;
 char resetsound=1;
 char startsound=1;
 char waittime=0;
+char mute=0;
 
 void setKernal(char _kernal) {
   GP4_bit=0;
@@ -85,6 +86,10 @@ void setWaitTime(char _waittime) {
   EEPROM_Write(0x04,waittime);
 }
 
+void setMuteSounds(char _waittime) {
+  EEPROM_Write(0x05,mute);
+}
+
 void toggleKernal(void) {
   kernalno++;
   kernalno&=0x03;
@@ -109,6 +114,12 @@ void toggleStartSound(void) {
   setStartSound(startsound);
 }
 
+void toggleMuteSounds(void) {
+  mute++;
+  mute&=0x01;
+  setMuteSounds(mute);
+}
+
 void toggleWaitTime(void) {
   waittime++;
   waittime&=0x01;
@@ -117,9 +128,9 @@ void toggleWaitTime(void) {
 
 void intres(void) {
   INTRST_N=1;
-  if(resetsound) RED_LED=~RED_LED;
+  if(resetsound && !mute) RED_LED=~RED_LED;
   delay_ms(200); // 200ms reset sound/blink
-  if(resetsound) RED_LED=~RED_LED;
+  if(resetsound && !mute) RED_LED=~RED_LED;
   delay_ms(50);
   INTRST_N=0;
 }
@@ -129,23 +140,23 @@ void setLED(void) {
 }
 
 void blinkLED(void) {
-  RED_LED=~RED_LED;
+  if(!mute) RED_LED=~RED_LED;
   delay_ms(100);
-  RED_LED=~RED_LED;
+  if(!mute) RED_LED=~RED_LED;
   delay_ms(100);
 }
 
 void init(void) {
   char _i;
   OPTION_REG=0;
-//  OPTION_REG=0b0001110; // about 1s watchdog timer.
-  WPU.WPU1=1;
-  CMCON=0x07; // digital IO
+  // OPTION_REG=0b0001110; // about 1s watchdog timer.
+  // WPU.WPU1=1;           // Weak pull-up register
+  CMCON=0x07;              // digital IO
 #ifdef P12F675
   ANSEL=0;
 #endif
   
-//ANSEL=0; // only defined for pic12f675
+  // ANSEL=0;              // only defined for pic12f675
   TRISIO=0b00001000;
   INTRST_N=0;
   RESET_N=1;
@@ -157,12 +168,14 @@ void init(void) {
   resetsound=EEPROM_READ(0x02);
   startsound=EEPROM_READ(0x03);
   waittime=EEPROM_READ(0x04);
+  mute=EEPROM_READ(0x05);
   GP3_bit=1; // presetting inputs for the debugger
   if(kernalno>3) kernalno=0;     // incase EEPROM garbage.
   if(driveno>1) driveno=0;       // incase EEPROM garbage.
   if(resetsound>1) resetsound=1; // incase EEPROM garbage.
   if(startsound>1) startsound=1; // incase EEPROM garbage.
   if(waittime>1) waittime=0;     // incase EEPROM garbage.
+  if(mute>1) mute=0;             // incase EEPROM garbage.
   setKernal(kernalno);
   setDrive(driveno);
   intres();
@@ -171,7 +184,7 @@ void init(void) {
   GPIE_bit=1;       // GPIO Interrupt enable, on
   GIE_bit=0;        // Globale interrupt enable, off
 
-  if(startsound) {
+  if(startsound && !mute) {
     for(_i=0; _i<5; _i++) {
       //asm { CLRWDT };
       RED_LED=1;
@@ -184,7 +197,7 @@ void init(void) {
 
 void main() {
   char i;
-//  asm { CLRWDT };
+  // asm { CLRWDT };
   init();
 
   while(1) {
@@ -209,20 +222,21 @@ void main() {
 
         // Bump cycle counter
         if (buttontimer > 10+waittime*10) {
-          cycle++; buttontimer=0;
+          if(cycle < 11) cycle++;
+          buttontimer=0;
           blinkLED();
         }
         break;
 
       case WAIT_RELEASE:
         switch (cycle) {
-          case 1:                  // 1-2 seconds
+          case 1:                  // 1 led/piezo blink cycles
             STATE=RESET;
             break;
-          case 2:                  // 2-3 seconds
+          case 2:                  // 2 blinks
             STATE=KERNAL_TOGGLE;
             break;
-          case 3:                  // 2-3 seconds
+          case 3:                  // 3 blinks
             STATE=DRIVE_TOGGLE;
             break;
           case 4:
@@ -250,6 +264,9 @@ void main() {
           case 10:
             STATE=WAITTIME_TOGGLE;
             break;
+          case 11:
+            STATE=MUTE_TOGGLE;
+            break;
           default:
             STATE=RESET;
             break;
@@ -273,6 +290,12 @@ void main() {
       case WAITTIME_TOGGLE:
         STATE=RESET;
         toggleWaitTime();
+        delay_ms(20);
+        break;
+
+      case MUTE_TOGGLE:
+        STATE=RESET;
+        toggleMuteSounds();
         delay_ms(20);
         break;
 
